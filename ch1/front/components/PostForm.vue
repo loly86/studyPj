b2a70f7eb5e794833831eee1ef3e8c8b5ec468e2
minsoo <minsoo@minsoonoMacBook-Air.local>
2021-02-28 22:29:19 +0900

    good commit1

---
+++ ch1/front/components/PostForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card>
+    <v-container>
+      <v-form>
+        <v-textarea outlned auto-grow clearable label="뭐임마" />
+      </v-form>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
